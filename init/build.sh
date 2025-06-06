42e6e0a7b42ffadee612e836695573eefde0c324
machsix <28209092+machsix@users.noreply.github.com>
2025-06-05 18:57:52 -0700

    v0.23.1-0

---
+++ init/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+spack external find binutils zlib xz cmake ncurses python diffutils pkgconf zlib
+spack install petsc@3.19.5+mpi+hypre+mumps+fortran %intel ^intel-oneapi-mkl ^intel-oneapi-mpi++classic-names
+spack mark -e hdf5 hypre parmetis mumps
+spack install -v hypre+mpi%nvhpc ^[virtuals=mpi,blas,lapack] nvhpc
+spack install kokkos@master +cuda +wrapper cuda_arch=86 %nvhpc
