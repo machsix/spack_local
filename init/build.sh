2acb7a22bc0f82b8ab851a282a90e24a530c7919
machsix <28209092+machsix@users.noreply.github.com>
2025-06-09 13:32:28 -0700

    update init

--- init/build.sh
+++ init/build.sh
@@ -4,3 +4,4 @@ spack install petsc@3.19.5+mpi+hypre+mumps+fortran %intel ^intel-oneapi-mkl ^int
 spack mark -e hdf5 hypre parmetis mumps
 spack install -v hypre+mpi%nvhpc ^[virtuals=mpi,blas,lapack] nvhpc
 spack install kokkos@master +cuda +wrapper cuda_arch=86 %nvhpc
+spack install hdf5@1.12.3+fortran+hl+mpi api=v110 %intel ^intel-oneapi-mpi++classic-names
