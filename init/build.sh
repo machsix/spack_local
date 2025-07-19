ff93a930a6db4439f5c329999758639b0a9b3f84
root <root@localhost.localdomain>
2025-07-19 19:28:52 -0400

    hypre+openmp

--- init/build.sh
+++ init/build.sh
@@ -4,3 +4,4 @@ spack install petsc@3.19.5+mpi+hypre+mumps+fortran %intel ^intel-oneapi-mkl ^int
 spack mark -e hdf5 hypre parmetis mumps
 spack install -v hypre+mpi%nvhpc ^[virtuals=mpi,blas,lapack] nvhpc
 spack install kokkos@master +cuda +wrapper cuda_arch=86 %nvhpc
+spack install -v hypre@2.33.0+mpi+openmp%nvhpc ^[virtuals=mpi,blas,lapack] nvhpc
