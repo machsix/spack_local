1768961ca409d3a2b52155cbbf593bad47820a21
machsix <28209092+machsix@users.noreply.github.com>
2025-07-19 19:31:08 -0400

    hdf5

--- init/build.sh
+++ init/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 spack external find binutils zlib xz cmake ncurses python diffutils pkgconf zlib
+spack install hdf5@1.12.3+fortran+hl+mpi api=v110 %intel ^intel-oneapi-mpi++classic-names
 spack install petsc@3.19.5+mpi+hypre+mumps+fortran %intel ^intel-oneapi-mkl ^intel-oneapi-mpi++classic-names
-spack mark -e hdf5 hypre parmetis mumps
+spack mark -e hypre parmetis mumps
 spack install -v hypre+mpi%nvhpc ^[virtuals=mpi,blas,lapack] nvhpc
-spack install kokkos@master +cuda +wrapper cuda_arch=86 %nvhpc
 spack install -v hypre@2.33.0+mpi+openmp%nvhpc ^[virtuals=mpi,blas,lapack] nvhpc
+spack install kokkos@master +cuda +wrapper cuda_arch=86 %nvhpc
