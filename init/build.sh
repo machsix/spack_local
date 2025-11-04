06efddaf275e406ce21f6fa003706e55cbd660fc
machsix <28209092+machsix@users.noreply.github.com>
2025-11-04 05:45:01 -0800

    hdf5%gcc: use system zlib

--- init/build.sh
+++ init/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # spack external find binutils zlib xz cmake ncurses python diffutils pkgconf zlib
 spack install -v hdf5@1.12.3+fortran+hl+mpi+cxx api=v110 %intel ^intel-oneapi-mpi++classic-names
-spack install -v hdf5@1.12.3+fortran+hl+mpi+cxx api=v110 %gcc ^intel-oneapi-mpi+generic-names
+spack install -v hdf5@1.12.3+fortran+hl+mpi+cxx api=v110 %gcc ^intel-oneapi-mpi+generic-names ^zlib%gcc
 spack install -v hypre@2.33.0+mpi+openmp %nvhpc ^[virtuals=mpi,blas,lapack] nvhpc
 spack install -v hypre@2.28.0%intel+fortran+mpi ^intel-oneapi-mpi++classic-names ^[virtuals=blas,lapack] intel-oneapi-mkl
 spack install -v kokkos@m4.7.00 +cuda +wrapper cuda_arch=86 %nvhpc
